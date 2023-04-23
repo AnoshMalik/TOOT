474dbe3c9c7d03ea46b94dd34db0e7b78520f10d
Roman-hal <85693082+Roman-Hal@users.noreply.github.com>
2023-04-23 15:10:12 +0100

    Using_prettier
    using prettier to resolve ESlint issues
    
    Co-Authored-By: azadehroshan <105980239+azadehroshan@users.noreply.github.com>
    Co-Authored-By: Anosh Malik <anoshmalik.am@gmail.com>
    Co-Authored-By: Yousef Shaikhan <105981004+youweb3@users.noreply.github.com>

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+//header component
 const Header = () => {
-  return (
-    <div><button>click me</button>Header</div>
-  );
+	return (
+		<div>
+			<button>click me</button>Header
+		</div>
+	);
 };
 
 export default Header;
