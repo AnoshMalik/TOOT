c3cf3d2f44d2c367ad694eda1a1c08c17f27f305
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-04-23 12:14:18 +0100

    Created_footer_and_header
    Add header and footer components, need to add logo and other materials in both.
    
    Co-Authored-By: Anosh Malik <anoshmalik.am@gmail.com>
    Co-Authored-By: Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
    Co-Authored-By: Yousef Shaikhan <105981004+youweb3@users.noreply.github.com>

---
+++ client/src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div><a href="#">About</a></div>
+  );
+}
+
+export default Header;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,43 +2,53 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Home.css";
-import logo from "./logo.svg";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+// import logo from "./logo.svg";
 
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
+	// const [message, setMessage] = useState("Loading...");
 
-	useEffect(() => {
-		fetch("/api")
-			.then((res) => {
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				return res.json();
-			})
-			.then((body) => {
-				setMessage(body.message);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
+	// useEffect(() => {
+	// 	fetch("/api")
+	// 		.then((res) => {
+	// 			if (!res.ok) {
+	// 				throw new Error(res.statusText);
+	// 			}
+	// 			return res.json();
+	// 		})
+	// 		.then((body) => {
+	// 			setMessage(body.message);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.error(err);
+	// 		});
+	// }, []);
 
-	return (
-		<main role="main">
-			<div>
-				<img
-					className="logo"
-					data-qa="logo"
-					src={logo}
-					alt="Just the React logo"
-				/>
-				<h1 className="message" data-qa="message">
-					{message}
-				</h1>
-				<Link to="/about/this/site">About</Link>
-			</div>
-		</main>
-	);
+	// return (
+	// 	<main role="main">
+	// 		<div>
+	// 			<img
+	// 				className="logo"
+	// 				data-qa="logo"
+	// 				src={logo}
+	// 				alt="Just the React logo"
+	// 			/>
+	// 			<h1 className="message" data-qa="message">
+	// 				{message}
+	// 			</h1>
+	// 			<Link to="/about/this/site">About</Link>
+	// 		</div>
+	// 	</main>
+	// );
+return (
+	<div>
+		<Header />
+		<h1>Suggestion Generator</h1>
+		<Footer />
+	</div>
+);
+
 }
 
 export default Home;
