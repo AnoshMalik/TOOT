29e993f312dc68fe830bbdb0216e278c57f672c1
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-04-26 16:19:31 +0100

    added_toot_title_on_header
    Co-Authored-By: Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
    Co-Authored-By: Anosh Malik <anoshmalik.am@gmail.com>
    Co-Authored-By: Yousef Shaikhan <105981004+youweb3@users.noreply.github.com>

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,21 +1,27 @@
 import React from "react";
 import logo from "../assets/cyf_brand.png";
-import { Navbar, Button, Nav, Col } from "react-bootstrap";
+import { Navbar, Button, Col } from "react-bootstrap";
 //header component
 const Header = () => {
 	return (
 		<>
-			<Navbar bg="light" expand="lg">
-				<Col xs={8}>
-					<img src={logo} alt="logo" style={{ width: "14em" }} />
+			<Navbar bg="light" expand="md" className="px-4">
+				<Col xs={5} md={3} lg={3} xl={3} xxl={2}>
+					<img src={logo} alt="logo" style={{ width: "100%" }} />
 				</Col>
-				<Col xs="auto">
-					<Nav>
-						<Button variant="outline-danger">Sign Out</Button>
-					</Nav>
+				<Col>
+					<h3 className="text-center font-weight-bold text-danger" >TOOT</h3>
+				</Col>
+				<Col lg={1} xl={1} className="ms-auto">
+					<Button variant="danger" style={{ width: "100px" }}>
+						Sign Out
+					</Button>
 				</Col>
 			</Navbar>
-			<div className="bg-danger py-2"></div>
+			<div
+				className="bg-danger py-3"
+				style={{ textAlign: "center", color: "white" }}
+			></div>
 		</>
 	);
 };
