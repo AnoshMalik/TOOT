36e0229f93430c44cd60d2bb888bd269ad56ffc1
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-04-26 14:50:17 +0100

    Added_Navbar_Logo_Signout

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
+import logo from "../assets/cyf_brand.png";
+import { Navbar, Button, Nav, Col } from "react-bootstrap";
 //header component
 const Header = () => {
 	return (
-		<div>
-			<button>click me</button>Header
-		</div>
+		<>
+			<Navbar bg="light" expand="lg">
+				<Col xs={8}>
+					<img src={logo} alt="logo" style={{ width: "14em" }} />
+				</Col>
+				<Col xs="auto">
+					<Nav>
+						<Button variant="outline-danger">Sign Out</Button>
+					</Nav>
+				</Col>
+			</Navbar>
+			<div className="bg-danger py-2"></div>
+		</>
 	);
 };
 
