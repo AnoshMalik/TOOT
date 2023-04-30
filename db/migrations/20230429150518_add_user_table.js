077ba5cc679f7983cde34d5d9688fc4668f2fe90
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-04-30 12:10:06 +0100

    Added_Database_Migration
    Co-Authored-By: Anosh Malik <anoshmalik.am@gmail.com>
    Co-Authored-By: Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
    Co-Authored-By: Yousef Shaikhan <105981004+youweb3@users.noreply.github.com>

--- db/migrations/20230429150518_add_user_table.js
+++ db/migrations/20230429150518_add_user_table.js
@@ -2,14 +2,27 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-
+exports.up = function (knex) {
+	return knex.schema
+		.createTable("users", function (table) {
+			table.increments();
+			table.string("username").notNullable();
+			table.string("name").notNullable();
+			table.integer("github_id").notNullable();
+		})
+		.createTable("history", function (table) {
+			table.increments();
+			table.string("input").notNullable();
+			table.string("output").notNullable();
+			table.integer("user_id").references("id").inTable("users");
+			table.timestamp("timestamp").notNullable().defaultTo(knex.fn.now());
+		});
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-
+exports.down = function (knex) {
+	return knex.schema.dropTable("users").dropTable("history");
 };
