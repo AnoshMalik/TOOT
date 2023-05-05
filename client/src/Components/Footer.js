cff57600ff88c09abd1da59fd061c270142f2d11
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-05-05 12:48:54 +0100

    Replaced_and_header
    The header and footer with Bootstrap React were embedded in the history page.
    In the footer, the fixed bottom in className also changed because it makes overlapped with right side textboxes from the bottom
    
    Co-Authored-By: Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
    Co-Authored-By: Anosh Malik <anoshmalik.am@gmail.com>
    Co-Authored-By: Yousef Shaikhan <105981004+youweb3@users.noreply.github.com>

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -4,7 +4,7 @@ import TeamLogo from "../assets/TeamLogo.png";
 const Footer = () => {
 	return (
 		<footer
-			className="bg-danger py-3 border border-dark fixed-bottom "
+			className="bg-danger py-3 border border-dark "
 			style={{ color: "red", display: "flex", justifyContent: "flex-end" }}
 		>
 			<Card.Img src={TeamLogo} style={{ width: "8%", marginRight: "1%" }} />
--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -1,16 +1,97 @@
-import React from "react";
-import Header from "../Components/History/Header";
-import Main from "../Components/History/Main";
-import Footer from "../Components/History/Footer";
+import React, { useState } from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import { BsSearch, BsSliders, BsSortDown, BsTrash3, BsX } from "react-icons/bs";
+import { FaAngleDown } from "react-icons/fa";
+import "./Main.css";
+
+
+
 
 const History = () => {
+		const obj = [
+			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+			"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		];
+	const [data, Setdata] = useState(obj);
 	return (
 		<>
 			<Header />
-			<Main />
+			<main>
+			<form className="search-input-form">
+				<textarea name="" id="" cols="30" rows="10"></textarea>
+				<button>
+					<FaAngleDown />
+				</button>
+				<textarea name="" id="" cols="30" rows="10"></textarea>
+			</form>
+			<section className="main-history">
+				<div className="main-history-header">
+					<h1>History</h1>
+				</div>
+				<div className="input-buttons-container">
+					<form className="history-sec-form">
+						<button>
+							<BsSearch />
+						</button>
+						<input type="text" />
+					</form>
+					<div className="history-btns">
+						<button>
+							<BsSliders />
+						</button>
+						<button>
+							<BsSortDown />
+						</button>
+						<button>
+							<BsTrash3 />
+						</button>
+					</div>
+				</div>
+				<ul className="history-list">
+					{data.map((text, index) => (
+						<li key={index} className="history-list-item">
+							<p>{text}</p>
+							<button>
+								<BsX />
+							</button>
+						</li>
+					))}
+				</ul>
+			</section>
+		</main>
             <Footer />
 		</>
 	);
 };
 
 export default History;
+
+
+
+
+
+
+
+
+
+
