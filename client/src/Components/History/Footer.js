641d0842d6cf8e6652d3ddd3c82d7adab3e80b48
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-05-05 10:45:32 +0100

    Created footer for history page

---
+++ client/src/Components/History/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AryaLogo from "../../assets/TeamLogo.png";
+import "./Footer.css";
+const Footer = () => {
+	return (
+		<div className="footer">
+			<img src={AryaLogo} alt="logo" />
+		</div>
+	);
+};
+
+export default Footer;
