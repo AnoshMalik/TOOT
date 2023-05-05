9af18cf6a122499c872d7668a8de1451d5ebd41a
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-05-05 18:21:54 +0100

    Moved_header-and-footer
    Co-Authored-By: Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
    Co-Authored-By: Anosh Malik <anoshmalik.am@gmail.com>
    Co-Authored-By: Yousef Shaikhan <105981004+youweb3@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import History from "./pages/History";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/history" element={<History />} />
-		<Route path="/about/this/site" element={<About />} />
-	</Routes>
+	<div>
+		<Header />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/history" element={<History />} />
+			<Route path="/about/this/site" element={<About />} />
+		</Routes>
+		<Footer />
+	</div>
 );
 
 export default App;
--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -1,97 +1,80 @@
 import React, { useState } from "react";
-import Header from "../Components/Header";
-import Footer from "../Components/Footer";
 import { BsSearch, BsSliders, BsSortDown, BsTrash3, BsX } from "react-icons/bs";
 import { FaAngleDown } from "react-icons/fa";
 import "./Main.css";
 
-
-
-
 const History = () => {
-		const obj = [
-			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-			"ZIS IZ MY SECOND ANTRY INTO THE APP",
-		];
+	const obj = [
+		"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"THAS AS MY 3RD ENTRY INTO THE CORRECTIONZ BUX",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+		"ZIS IZ MY SECOND ANTRY INTO THE APP",
+	];
 	const [data, Setdata] = useState(obj);
 	return (
 		<>
-			<Header />
 			<main>
-			<form className="search-input-form">
-				<textarea name="" id="" cols="30" rows="10"></textarea>
-				<button>
-					<FaAngleDown />
-				</button>
-				<textarea name="" id="" cols="30" rows="10"></textarea>
-			</form>
-			<section className="main-history">
-				<div className="main-history-header">
-					<h1>History</h1>
-				</div>
-				<div className="input-buttons-container">
-					<form className="history-sec-form">
-						<button>
-							<BsSearch />
-						</button>
-						<input type="text" />
-					</form>
-					<div className="history-btns">
-						<button>
-							<BsSliders />
-						</button>
-						<button>
-							<BsSortDown />
-						</button>
-						<button>
-							<BsTrash3 />
-						</button>
+				<form className="search-input-form">
+					<textarea name="" id="" cols="30" rows="10"></textarea>
+					<button>
+						<FaAngleDown />
+					</button>
+					<textarea name="" id="" cols="30" rows="10"></textarea>
+				</form>
+				<section className="main-history">
+					<div className="main-history-header">
+						<h1>History</h1>
 					</div>
-				</div>
-				<ul className="history-list">
-					{data.map((text, index) => (
-						<li key={index} className="history-list-item">
-							<p>{text}</p>
+					<div className="input-buttons-container">
+						<form className="history-sec-form">
 							<button>
-								<BsX />
+								<BsSearch />
 							</button>
-						</li>
-					))}
-				</ul>
-			</section>
-		</main>
-            <Footer />
+							<input type="text" />
+						</form>
+						<div className="history-btns">
+							<button>
+								<BsSliders />
+							</button>
+							<button>
+								<BsSortDown />
+							</button>
+							<button>
+								<BsTrash3 />
+							</button>
+						</div>
+					</div>
+					<ul className="history-list">
+						{data.map((text, index) => (
+							<li key={index} className="history-list-item">
+								<p>{text}</p>
+								<button>
+									<BsX />
+								</button>
+							</li>
+						))}
+					</ul>
+				</section>
+			</main>
 		</>
 	);
 };
 
 export default History;
-
-
-
-
-
-
-
-
-
-
