419bce44223e19f8d84c95e72144b8720033214a
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-05-05 10:42:12 +0100

    Added new route for history page

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import History from "./pages/History";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
+		<Route path="/history" element={<History />} />
 		<Route path="/about/this/site" element={<About />} />
 	</Routes>
 );
---
+++ client/src/pages/History.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Components/History/Header";
+import Main from "../Components/History/Main";
+import Footer from "../Components/History/Footer";
+
+const History = () => {
+	return (
+		<>
+			<Header />
+			<Main />
+            <Footer />
+		</>
+	);
+};
+
+export default History;
